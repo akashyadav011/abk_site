8ced06e17af753be3d64d505fad02e05a24fb18a
Akash Yadav <ak.techsage@gmail.com>
2025-07-24 23:58:06 +0530

    Update project

--- abk_site/settings.py
+++ abk_site/settings.py
@@ -119,7 +119,11 @@ USE_TZ = True
 import os
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Optional, if you have custom static folders
+
+# Required for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
